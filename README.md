d7e7cf165eaa8dec040bfee60b7a835b15304b7e
이치연 <leechi2228@gmail.com>
2023-08-26 22:30:07 +0900

    ✏️ How to make / change useState

--- README.md
+++ README.md
@@ -16,7 +16,28 @@ Don't input class! Why? JSX is javascript .
 class is js syntax. so we must input className.
 
 2. {variable} 
-r
+
 3. style tag style = {{fontSize:'30px'}} 
 if i change font-size i must input fontSize 
+
+
+<h1>useState</h1>
+
+### How to make useState
+1. import {useState} from 'react'
+2. Destructuring Syntax 
+let [title, setTitle] = useState('recommend man's coat')
+
+### How to change useState
+1. event handler 
+onClick( function )
+onChange( function )
+2. let [title, setTitle] = useState('Hello useState')
+first is variable! and second is function that change state
+so how to change useState ? 
+onClick(()=>{ return setTitle(title = 'How to change useState?' )})
+3. How to change array state
+If state is array or object we must make shallow copy and change!
+To change array, we use spread operator.
+Spread operator is ... and remove bracket!
 
--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
   let post = 'Learn Ract'
+  let [title, setTitle] = useState(['타자 코트 추천', '맛집 추천', '맛있는 과자 추천'])
+  console.log(title[0])
+  let [like, setLike] = useState(0)
+  const updateLike = () => {
+    setLike(like++)
+  }
   return (
     <div className="App">
       <nav className="black-nav">
         <h4>leechi's blog</h4>
       </nav>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{title[0]} <span onClick={updateLike}>❤️</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <button onClick={() => {
+        let copy = [...title]
+        copy[0] = '여자코트추천'
+        setTitle(copy)
+        
+      }}>change button</button>
+      // 가나다순 정렬하기 버튼
+      <button onClick={() => {
+        let copy = [...title]
+        console.log(copy)
+        copy.sort()
+        setTitle(copy)
+      }
+      }>정렬하기</button>
     </div>
   );
 }
