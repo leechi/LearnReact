779fe6b1d5c25688e222e7671a1d6bd48b182c3d
이치연 <leechi2228@gmail.com>
2023-08-27 15:11:59 +0900

    How to use eventHandler(onChange)

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,8 +6,10 @@ function App() {
   let post = 'Learn Ract'
   let [title, setTitle] = useState(['타자 코트 추천', '맛집 추천', '맛있는 과자 추천'])
   let [modal, setModal] = useState(false)
-  let [like, setLike] = useState([0, 0, 0])
+  let [like, setLike] = useState([0,0,0])
+  let [day, setDay] = useState(['3월 28일 발행', '2월 22일 발행', '2월 4일 발행'])
   let [change, setChange] = useState(0)
+  let [input, setInput] = useState('')
   const updateLike = () => {
     setLike(like +=1)
   }
@@ -20,15 +22,21 @@ function App() {
       {title.map((data, i) => {
         return (
           <div key={i} className="list" >
-            <h4 onClick={() => {
+            <h4 onClick={(e) => {
               setModal(true) 
               setChange(i)
-            }}>{data} <span onClick={() => {
+            }}>{data} <span onClick={(e) => {
+                e.stopPropagation();
               let copy = [...like]
               copy[i] = copy[i] + 1
               setLike(copy)
             }}>❤️</span> {like[i]}</h4>
-        <p>2월 17일 발행</p>
+            <p>{day[i]}</p>
+            <button onClick={(e) => {
+              let copy = [...title]
+              copy.splice(i,1)
+              setTitle(copy)
+            }}>삭제</button>
       </div>
         )
       })}
@@ -47,6 +55,23 @@ function App() {
         setTitle(copy)
       }
       }>정렬하기</button>
+      <input type="text" required onChange={(e) => { setInput(e.target.value); }} />
+
+      <button onClick={(e) => {
+        if (input.trim() !== '') {
+          let copy = [...title]
+          copy.unshift(input)
+          setTitle(copy)
+          let copy1 = [...like]
+          copy1.unshift(0)
+          setLike(copy1)
+          // let copy2 = [...day]
+          day.unshift('2월22일')
+          setDay(day)
+        }
+        
+      }}>추가하기</button>
+
       {modal === true ? 
         <Modal change={change}  setTitle={setTitle} title={title} />
       :
